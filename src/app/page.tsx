1e8929f38f583690d7f432a6dd22968e2f4c518f
nokiaorzech <pzubowski12@gmail>
2024-03-09 23:24:44 +0100

    fdsgf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>szczepfuck</h1>
         <p>jebac dupafsdgfsfdf scepanik💩💩💩💩💩💩</p>
         <div className="dupa">tegdfgwsrgwrFgo</div>
+        <p>scepan fdsfgej</p>
       </div>
     </main>
+    </>
   );
 }
