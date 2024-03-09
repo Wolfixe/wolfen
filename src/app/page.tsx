c97b2557d6c771dc8b22711d85602dccb056f7a4
nokiaorzech <pzubowski12@gmail>
2024-03-09 22:28:30 +0100

    first push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1>szczepfuck</h1>
+        <p>jebac dupa scepanik💩💩💩💩💩💩</p>
       </div>
     </main>
   );
